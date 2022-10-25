58843b6abbefdb47608bcfb6a0e8d62273218555
mentalgear <tomfaber7@gmail.com>
2022-10-25 20:54:48 +0200

    docs: add accessibility & testability attribute ex

--- example/InputError.svelte
+++ example/InputError.svelte
@@ -15,7 +15,13 @@
   $: errors = $result.getErrors()[name]
 </script>
 
-<small data-testid={`error-${name}`}>
+<small role="alert" data-testid={`error-${name}`}>
+  <!-- role="alert" ARIA about:
+    1. for accessibility: add to the element containing the validation error
+    and screenreaders will read the error out for the user
+    2. for testability DX: can be used in tests to check if the validation
+    message appears (for example using testing-library's .getByRole('alert')
+  -->
   {#if errors}
     <!--
       There is no need to display all errors since normally the first
